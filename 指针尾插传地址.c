03a5e42ad4e5862255ff7fd908c751ef2005dcd0
明明丶 <42541837+theromin@users.noreply.github.com>
2018-12-19 13:15:15 +0800

    Create 指针尾插传地址.c

---
+++ 指针尾插传地址.c
@@ -0,0 +1,60 @@
+/*
+键盘输入若干个整数（键盘 ^Z结束输入），按照输入顺序创建链表。
+
+链表创建完成后，依序访问链表每个结点并输出各结点数据值及该结点的地址。
+
+注意：可实现两种情形的链表：
+
+a.不带附加头结点的单链表。
+
+b. 带附加头结点的单链表。
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct preList
+{
+	int num;
+	struct preList *next;
+}List;
+
+void CreateList(List **head);
+void read(List *head);
+
+int main()
+{
+	List *p=NULL;
+	CreateList(&p);
+	read(p);
+	system("pause");
+	return 0;
+}
+
+void CreateList(List **head)
+{
+	int num;
+	List *tail, *p;
+	while (~scanf("%d", &num))
+	{
+		p = (List*)malloc(sizeof(List));
+		p->num = num;
+		p->next = NULL;
+		if (*head == NULL)
+			*head = p;
+		else
+			tail->next = p;
+		tail = p;
+	}
+}
+
+void read(List *head)
+{
+	List *p = head;
+	if (head == NULL)return;
+	do
+	{
+		printf("%d %x\n", p->num, &p->num);
+		p = p->next;
+	} while (p!= NULL);
+}
